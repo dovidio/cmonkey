81971c132ca4e3f5d5a7bb9ee1001411b797e796
Umberto D'Ovidio <umberto.dovidio@gmail.com>
2025-11-13 17:03:56 +0100

    formatting

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
+#include "repl.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "repl.h"
 
 int main() {
-    printf("Hello! This is the Monkey programming language!\n");
-    printf("Feel free to type in commands\n");
-    
-    start_repl(stdin, stdout);
-    
-    return 0;
+  printf("Hello! This is the Monkey programming language!\n");
+  printf("Feel free to type in commands\n");
+
+  start_repl(stdin, stdout);
+
+  return 0;
 }
